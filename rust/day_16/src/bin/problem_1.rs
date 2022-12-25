3b87d7f84122dca219283ca05a6fdfe2617d0aa1
Tobias Walle <tobias.walle@maibornwolff.de>
2022-12-25 10:05:44 +0100

    day 16 failed attempt 2

--- rust/day_16/src/bin/problem_1.rs
+++ rust/day_16/src/bin/problem_1.rs
@@ -1,11 +1,12 @@
 use std::{
-    cmp::Reverse,
+    cmp::{min, Reverse},
     collections::{BTreeSet, HashMap, HashSet},
     iter::Rev,
     rc::Rc,
 };
 
 use eyre::{bail, Context, ContextCompat, Result};
+use itertools::Itertools;
 use nom::{
     branch::alt,
     bytes::complete::tag,
@@ -26,134 +27,83 @@ fn main() -> Result<()> {
     Ok(())
 }
 
+const MAX_MINUTES: u32 = 30;
+
 fn solve_problem(input: &str) -> Result<u32> {
     let valves = parsing::parse_with_nom(input, parse_valves)?;
-    let best_tree = build_decision_trees(valves, 30);
-    Ok(best_tree.real_pressure)
-}
-
-fn build_decision_trees(valves: Vec<Valve>, max_minutes: u32) -> Rc<DecisionTree> {
-    let valves: HashMap<_, _> = valves.into_iter().map(|v| (v.id.clone(), v)).collect();
-    let valves = Rc::new(valves);
-    let initial: Vec<_> = valves
-        .values()
-        .map(|valve| {
-            Rc::new(DecisionTree {
-                valve_id: valve.id.clone(),
-                minutes: 1,
-                real_pressure: 0,
-                pontential_pressure: 0,
-                opened: BTreeSet::new(),
-                parent: None,
-            })
-        })
-        .collect();
-    find_best_tree(initial, valves, max_minutes).unwrap()
-}
-
-fn find_best_tree(
-    start: Vec<Rc<DecisionTree>>,
-    valves: Rc<HashMap<ValveId, Valve>>,
-    max_minutes: u32,
-) -> Result<Rc<DecisionTree>> {
-    let mut queue = PriorityQueue::new();
-    let mut scores = HashMap::new();
-
-    for item in start {
-        queue.push(item.clone(), item.pontential_pressure);
-        scores.insert(item.key(), item.real_pressure);
-    }
-
-    while !queue.is_empty() {
-        let (current, _) = queue.pop().unwrap();
-        if current.minutes == 30 {
-            return Ok(current);
+    let valves: ValvesById = valves.into_iter().map(|v| (v.id.clone(), v)).collect();
+    let mut paths = Vec::new();
+    for from in valves.keys() {
+        for to in valves.keys() {
+            let Some(path) = find_path(&valves, from, to) else { continue;};
+            paths.push(path);
         }
+    }
+    let paths = paths.into_iter().into_group_map_by(|p| p.from.clone());
+    let mut current_valve = valves.values().max_by_key(|v| v.flow_rate).unwrap();
+    let mut opened = HashSet::from([current_valve.id.clone()]);
+    let mut total_flow_rate = current_valve.flow_rate;
+    let mut pressure = 0;
+    let mut minute: u32 = 3;
 
-        for neighbour in get_next_decision_trees(&current, &valves, max_minutes) {
-            let tentative_score = neighbour.real_pressure;
-            let neighbour_score = scores.get(&neighbour.key());
-            if neighbour_score.is_none() || tentative_score > *neighbour_score.unwrap() {
-                scores.insert(neighbour.key(), tentative_score);
-                queue.push(neighbour.clone(), neighbour.real_pressure);
+    loop {
+        let minutes_left = MAX_MINUTES - minute;
+        let next_path = paths[&current_valve.id]
+            .iter()
+            .filter(|path| !opened.contains(&path.to))
+            .filter(|path| path.minutes < minutes_left)
+            .max_by_key(|path| calculate_path_score(path, minutes_left));
+        let Some(next_path) = next_path else { break };
+
+        let next_valve = &valves[&next_path.to];
+        let minutes_move_and_open = next_path.minutes + 1;
+        pressure += total_flow_rate * minutes_move_and_open;
+        minute += minutes_move_and_open;
+        total_flow_rate += next_valve.flow_rate;
+        opened.insert(next_valve.id.clone());
+        current_valve = next_valve;
+    }
+    let minutes_left = MAX_MINUTES - minute;
+    pressure += minutes_left * total_flow_rate;
+    Ok(pressure)
+}
+
+fn calculate_path_score(path: &Path, minutes_left: u32) -> Option<u32> {
+    if minutes_left < path.minutes {
+        return None;
+    }
+    let score = (minutes_left - path.minutes) * path.flow_rate;
+    Some(score)
+}
+
+fn find_path(valves: &ValvesById, from: &ValveId, to: &ValveId) -> Option<Path> {
+    let mut queue = vec![vec![from.clone()]];
+    while let Some(path) = queue.pop() {
+        let node = path.last().unwrap();
+        if node == to {
+            return Some(compute_path(valves, &path));
+        }
+        for connection in &valves[node].connections {
+            if path.contains(connection) {
+                continue;
             }
+            let mut new_path = path.clone();
+            new_path.push(connection.clone());
+            queue.push(new_path);
         }
     }
-
-    bail!("No path found!")
+    None
 }
 
-fn find_best_decision_tree(
-    start: Rc<DecisionTree>,
-    valves: Rc<HashMap<ValveId, Valve>>,
-    max_minutes: u32,
-    cache: &mut HashMap<Rc<DecisionTree>, Rc<DecisionTree>>,
-) -> Rc<DecisionTree> {
-    let mut queue = PriorityQueue::new();
-    queue.push(start.clone(), start.minutes);
-    let mut possible_decision_trees = Vec::new();
-    while let Some((next, _)) = queue.pop() {
-        if cache.contains_key(&next) {
-            continue;
-        }
-        for next_decision in get_next_decision_trees(&next, &valves, max_minutes) {
-            match next_decision.minutes.cmp(&max_minutes) {
-                std::cmp::Ordering::Equal => {
-                    possible_decision_trees.push(next_decision.clone());
-
-                    let mut parent = &next_decision.parent;
-                    while let Some(tree) = parent {
-                        cache.insert(tree.clone(), next_decision.clone());
-                        parent = &tree.parent;
-                    }
-                }
-                std::cmp::Ordering::Less => {
-                    queue.push(next_decision.clone(), next_decision.minutes);
-                }
-                std::cmp::Ordering::Greater => (),
-            }
-        }
+fn compute_path(valves: &ValvesById, path: &[ValveId]) -> Path {
+    let to = path.last().unwrap();
+    Path {
+        from: path[0].clone(),
+        to: to.clone(),
+        flow_rate: valves[to].flow_rate,
+        minutes: (path.len() - 1) as u32,
+        path: path.to_vec(),
     }
-    possible_decision_trees
-        .into_iter()
-        .max_by_key(|tree| tree.real_pressure)
-        .unwrap()
-}
-
-fn get_next_decision_trees(
-    start: &Rc<DecisionTree>,
-    valves: &HashMap<ValveId, Valve>,
-    max_minutes: u32,
-) -> Vec<Rc<DecisionTree>> {
-    let valve = &valves[&start.valve_id];
-    let mut decisions = Vec::new();
-    let new_pressure: u32 = start.opened.iter().map(|v| valves[v].flow_rate).sum();
-    let real_pressure = start.real_pressure + new_pressure;
-    let minutes = start.minutes + 1;
-    let minutes_left = max_minutes - minutes;
-    if !start.opened.contains(&start.valve_id) {
-        let mut opened = start.opened.clone();
-        opened.insert(start.valve_id.clone());
-        decisions.push(Rc::new(DecisionTree {
-            valve_id: start.valve_id.clone(),
-            minutes,
-            real_pressure,
-            pontential_pressure: real_pressure + valve.flow_rate * minutes_left,
-            opened,
-            parent: Some(start.clone()),
-        }));
-    }
-    for connection_id in &valve.connections {
-        decisions.push(Rc::new(DecisionTree {
-            valve_id: connection_id.clone(),
-            minutes,
-            real_pressure,
-            pontential_pressure: real_pressure,
-            opened: start.opened.clone(),
-            parent: Some(start.clone()),
-        }));
-    }
-    decisions
 }
 
 fn parse_valves(input: &str) -> IResult<&str, Vec<Valve>> {
@@ -186,47 +136,17 @@ fn parse_valve(input: &str) -> IResult<&str, Valve> {
     ))
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-struct DecisionTree {
-    valve_id: ValveId,
-    minutes: u32,
-    pontential_pressure: u32,
-    real_pressure: u32,
-    opened: BTreeSet<ValveId>,
-    parent: Option<Rc<DecisionTree>>,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-struct DecisionTreeKey {
-    valve_id: ValveId,
-    minutes: u32,
-    opened: BTreeSet<ValveId>,
-}
-
-type Chain = Vec<ValveId>;
-
-impl DecisionTree {
-    fn chain(self: Rc<Self>) -> Vec<ValveId> {
-        let mut chain = vec![];
-        let mut next = Some(self);
-        while let Some(tree) = next {
-            chain.push(tree.valve_id.clone());
-            next = tree.parent.clone();
-        }
-        chain
-    }
-
-    fn key(&self) -> DecisionTreeKey {
-        DecisionTreeKey {
-            valve_id: self.valve_id.clone(),
-            minutes: self.minutes,
-            opened: self.opened.clone(),
-        }
-    }
-}
+type ValvesById = HashMap<ValveId, Valve>;
+type Paths<'a> = HashMap<(&'a ValveId, &'a ValveId), Path>;
 
 #[derive(Debug, Clone)]
-struct Problem {}
+struct Path {
+    from: ValveId,
+    to: ValveId,
+    path: Vec<ValveId>,
+    flow_rate: u32,
+    minutes: u32,
+}
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
 struct ValveId(String);
