edcfac919a8cb873f06a411d79617c6c75eb16e0
Tobias Walle <tobias.walle@maibornwolff.de>
2022-12-29 11:54:19 +0100

    solve day 17 problem 2

--- rust/day_17/src/bin/problem_2.rs
+++ rust/day_17/src/bin/problem_2.rs
@@ -1,4 +1,5 @@
 use std::{
+    any::Any,
     collections::{HashMap, HashSet},
     thread,
     time::Duration,
@@ -11,12 +12,12 @@ use utils::{is_debugging, read_input_file_as_string};
 fn main() -> Result<()> {
     let input = read_input_file_as_string().context("Cannot read input")?;
 
-    let result = solve_problem(&input, 2022).context("Failed to solve problem")?;
+    let result = solve_problem(&input, 1_000_000_000_000).context("Failed to solve problem")?;
     println!("{result}");
     Ok(())
 }
 
-fn solve_problem(input: &str, settled_pieces_until_end: usize) -> Result<i32> {
+fn solve_problem(input: &str, settled_pieces_until_end: usize) -> Result<i64> {
     let mut game = parse(input, settled_pieces_until_end)?;
     game.render();
     while game.tick() {
@@ -72,6 +73,7 @@ fn parse(input: &str, settled_pieces_until_end: usize) -> Result<Game> {
         piece_types,
         next_piece_type_index: 1,
         settled: HashMap::new(),
+        settled_pieces: Vec::new(),
         height: 0,
         settled_pieces_count: 0,
     };
@@ -82,28 +84,52 @@ fn parse(input: &str, settled_pieces_until_end: usize) -> Result<Game> {
 #[derive(Debug, Clone)]
 struct Game {
     pub settled_pieces_until_end: usize,
-    pub width: i32,
+    pub width: i64,
+    pub height: i64,
     pub piece_types: Vec<Piece>,
     pub next_piece_type_index: usize,
     pub directions: Vec<Direction>,
     pub last_direction: Option<Direction>,
     pub next_direction_index: usize,
     pub settled: HashMap<Point, PieceTypeId>,
-    pub height: i32,
+    pub settled_pieces: Vec<SettledPiece>,
     pub settled_pieces_count: usize,
     pub falling: Piece,
 }
 
 impl Game {
     fn tick(&mut self) -> bool {
+        if self.settled_pieces_count == 10_000 {
+            let pattern = self.find_repeating_pattern().expect("No pattern found");
+            println!(
+                "Apply repeating pattern with {} pieces.",
+                pattern.settled_pieces.len()
+            );
+            self.apply_pattern_to_speedup_simulation(&pattern);
+            println!(
+                "After: count={} height={}",
+                self.settled_pieces_count, self.height
+            );
+        }
+
         let is_settled = self.try_moves().is_err();
 
         if is_settled {
             self.settled_pieces_count += 1;
+            let mut new_height = self.height;
             for point in &self.falling.points {
+                new_height = new_height.max(point.y + 1);
                 self.settled.insert(*point, self.falling.type_id);
-                self.height = self.height.max(point.y + 1);
             }
+            let settled_piece = SettledPiece {
+                type_id: self.falling.type_id,
+                piece_type_index: self.current_piece_type_index(),
+                direction_index: self.current_direction_index(),
+                position_x: self.falling.position_x(),
+                height_diff: new_height - self.height,
+            };
+            self.settled_pieces.push(settled_piece);
+            self.height = new_height;
 
             let new_piece_type = self.piece_types[self.next_piece_type_index].clone();
             self.next_piece_type_index = (self.next_piece_type_index + 1) % self.piece_types.len();
@@ -112,6 +138,18 @@ impl Game {
         }
 
         self.settled_pieces_count < self.settled_pieces_until_end
+    }
+
+    fn current_direction_index(&self) -> usize {
+        self.next_direction_index
+            .checked_sub(1)
+            .unwrap_or(self.directions.len())
+    }
+
+    fn current_piece_type_index(&self) -> usize {
+        self.next_piece_type_index
+            .checked_sub(1)
+            .unwrap_or(self.piece_types.len())
     }
 
     fn try_moves(&mut self) -> Result<()> {
@@ -146,6 +184,67 @@ impl Game {
         piece
     }
 
+    fn find_repeating_pattern(&self) -> Option<Pattern> {
+        let mut pattern = Vec::new();
+        let last_piece = self.settled_pieces.last()?;
+        let last_piece_index = self.settled_pieces.len() - 1;
+        let mut expected_piece_index = last_piece_index;
+        for piece in self.settled_pieces.iter().skip(1).rev() {
+            if piece == last_piece && !pattern.is_empty() {
+                break;
+            }
+            let Some(expected_piece) = self.settled_pieces.get(expected_piece_index) else {
+                return None;
+            };
+            if piece == expected_piece {
+                pattern.push(expected_piece);
+                expected_piece_index -= 1;
+            } else {
+                pattern.clear();
+                expected_piece_index = last_piece_index;
+            }
+        }
+        if pattern.is_empty() {
+            None
+        } else {
+            Some(Pattern {
+                height_diff: pattern.iter().map(|piece| piece.height_diff).sum(),
+                settled_pieces: pattern.into_iter().rev().cloned().collect(),
+            })
+        }
+    }
+
+    fn apply_pattern_to_speedup_simulation(&mut self, pattern: &Pattern) {
+        let pieces_left = self.settled_pieces_until_end - self.settled_pieces_count;
+        let times_to_apply_pattern = pieces_left / pattern.settled_pieces.len();
+        let height_diff = times_to_apply_pattern as i64 * pattern.height_diff;
+
+        self.settled_pieces_count += times_to_apply_pattern * pattern.settled_pieces.len();
+        self.height += height_diff;
+        self.falling.points = self
+            .falling
+            .points
+            .iter()
+            .map(|point| Point {
+                y: point.y + height_diff,
+                ..*point
+            })
+            .collect();
+        self.settled = self
+            .settled
+            .iter()
+            .map(|(point, piece)| {
+                (
+                    Point {
+                        y: point.y + height_diff,
+                        ..*point
+                    },
+                    *piece,
+                )
+            })
+            .collect();
+    }
+
     fn render(&self) {
         if !is_debugging() {
             return;
@@ -176,6 +275,11 @@ impl Game {
     }
 }
 
+struct Pattern {
+    settled_pieces: Vec<SettledPiece>,
+    height_diff: i64,
+}
+
 #[derive(Debug, Clone)]
 enum Direction {
     Right,
@@ -183,7 +287,7 @@ enum Direction {
     Down,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum PieceTypeId {
     A,
     B,
@@ -204,6 +308,15 @@ impl PieceTypeId {
     }
 }
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct SettledPiece {
+    type_id: PieceTypeId,
+    direction_index: usize,
+    position_x: i64,
+    height_diff: i64,
+    piece_type_index: usize,
+}
+
 #[derive(Debug, Clone)]
 struct Piece {
     type_id: PieceTypeId,
@@ -217,15 +330,13 @@ impl Piece {
             for (x, cell) in line.iter().enumerate() {
                 assert!(matches!(*cell, 0 | 1));
                 if *cell == 1 {
-                    points.insert(Point::new(x as i32, (H - 1 - y) as i32));
+                    points.insert(Point::new(x as i64, (H - 1 - y) as i64));
                 }
             }
         }
         Self { type_id, points }
     }
-}
 
-impl Piece {
     pub fn shift(&self, game: &Game, direction: &Direction) -> Option<Piece> {
         let points: Option<HashSet<Point>> = self
             .points
@@ -256,16 +367,20 @@ impl Piece {
         }
         false
     }
+
+    fn position_x(&self) -> i64 {
+        self.points.iter().map(|point| point.x).min().unwrap_or(0)
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 struct Point {
-    x: i32,
-    y: i32,
+    x: i64,
+    y: i64,
 }
 
 impl Point {
-    pub fn new(x: i32, y: i32) -> Self {
+    pub fn new(x: i64, y: i64) -> Self {
         Self { x, y }
     }
 
@@ -321,10 +436,18 @@ mod tests {
     }
 
     #[test]
-    fn test_example_with_2022() {
+    fn test_example_with_11_000() {
         let input = read_to_string("./example.txt").unwrap();
 
-        let result = solve_problem(&input, 2022).unwrap();
-        assert_eq!(result, 3068);
+        let result = solve_problem(&input, 11_000).unwrap();
+        assert_eq!(result, 16661);
+    }
+
+    #[test]
+    fn test_example_with_1_trillion() {
+        let input = read_to_string("./example.txt").unwrap();
+
+        let result = solve_problem(&input, 1_000_000_000_000).unwrap();
+        assert_eq!(result, 1514285714288);
     }
 }
